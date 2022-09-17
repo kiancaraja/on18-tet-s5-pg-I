5a97c0ca5f15570362dcbaad951561f6f38a6a49
kiancaraja <emsmms42@gmail.com>
2022-09-17 16:09:05 -0300

    exercicio resolvido

--- exercicios/para-sala/revisao/exercicio2.js
+++ exercicios/para-sala/revisao/exercicio2.js
@@ -1,1 +1,16 @@
 // Armazene uma soma entre dois números e mostre no console.log();
+
+let soma1 = 8
+
+let soma2 = 12
+
+let soma3 = 122
+
+let Resultado = soma1 + soma2 + soma3
+
+let Resultado = soma1 + soma2 + soma3
+
+console.log("A soma de " ,soma1,soma2,"e", soma3 ,"será igual" ,Resultado)
+
+console.log(soma1 , soma2 , "e" , soma3 , Resultado)
+
--- exercicios/para-sala/revisao/exercicio3.js
+++ exercicios/para-sala/revisao/exercicio3.js
@@ -1,1 +1,28 @@
 // Armazene separadamente, todos os tipos de dados que aprendeu até hoje.
+
+let ArmazenarTiposDeDados = "TiposDeDados" 
+
+let texto = "texto" // é um tipo de string
+
+let numero = 22
+
+let boolean = false
+
+let undefined = indefinido // representa um valor primitivo do JS
+
+null =
+
+console.log ("Resultado do exercício:", "tiposdedados" ); 
+
+let texto = "texto" // é um tipo de string
+
+let numero = 22
+
+let boolean = false
+
+let undefined = indefinido // representa um valor primitivo do JS
+
+null =
+
+console.log ("Resultado do exercício:", "tiposdedados" ); 
+
